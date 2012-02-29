f0e9f2bfa1619739a8ef12eed8ae9220e7af84f4
Nikola Skoric <nskoric@gmail.com>
2012-02-29 11:51:42 +0100

    Moving unified version to old name.

--- petrol_cijena.py
+++ petrol_cijena.py
@@ -5,17 +5,25 @@ from xml.dom import minidom
 
 def auth():
     '''The method to do HTTPBasicAuthentication'''
-    if len(sys.argv) < 2:
-        print "Usage: python " + sys.argv[0] + " currencycode"
+    if len(sys.argv) < 3:
+        print "Usage: python " + sys.argv[0] + " country currencycode"
         exit()
 
-    currencycode = sys.argv[1]
+    country = sys.argv[1]
+    currencycode = sys.argv[2]
     _URL = "http://www.petrol.si/api/gas_prices.xml"
 
     opener = urllib.FancyURLopener()
     f = opener.open(_URL)
     feed = f.read()
-    return feed, currencycode
+    return feed, country, currencycode
+
+def map_country_code(country_code):
+    if country_code == 'HR':
+        return 'Croatia'
+    elif country_code == 'SI':
+        return 'Slovenia'
+    return None
 
 def getChildByAttribute(node, attribute, value):
     for n in node.childNodes:
@@ -24,24 +32,25 @@ def getChildByAttribute(node, attribute, value):
 
     return None
 
-def parseXML(xml, currencycode):
+def parseXML(xml, country):
     xmldoc = minidom.parseString(xml)
-    slovenia = getChildByAttribute(xmldoc.firstChild, 'label', 'Slovenia')
+    slovenia = getChildByAttribute(xmldoc.firstChild, 'label', country)
     f95 = getChildByAttribute(slovenia, 'type', '95')
     priceType = getChildByAttribute(f95, 'type', 'normal')
     price = getChildByAttribute(priceType, 'type', 'price')
     return price.firstChild.toxml()
 
-def cijena_EUR(xml, currencycode):
-    return float(parseXML(xml, currencycode))
-
-def cijena_HRK(eur):
-    return (eur * EUR_2_HRK.getEUR_2_HRK())
+def cijena(xml, country):
+    return float(parseXML(xml, country))
 
 if __name__ == "__main__":
-    xml, currencycode = auth()
-    eur = cijena_EUR(xml, currencycode)
-    if currencycode == "EUR":
-        print "%.3f" % eur
-    else:
-        print "%.3f" % cijena_HRK(eur)
+    xml, country, currencycode = auth()
+    base_price = float(cijena(xml, map_country_code(country)))
+    if (country, currencycode) == ('HR', 'HRK'):
+        print "%.2f" % base_price
+    elif (country, currencycode) == ('HR', 'EUR'):
+        print "%.2f" % (base_price / EUR_2_HRK.getEUR_2_HRK())
+    elif (country, currencycode) == ('SI', 'HRK'):
+        print "%.2f" % (base_price * EUR_2_HRK.getEUR_2_HRK())
+    elif (country, currencycode) == ('SI', 'EUR'):
+        print "%.2f" % base_price
